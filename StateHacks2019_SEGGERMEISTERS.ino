8ab7cffbe196a81247171a218ac11168bc969ad2
SEGGERMEISTERS <57843680+SEGGERMEISTERS@users.noreply.github.com>
2019-11-16 13:42:11 -0800

    Add files via upload
    version 2

--- StateHacks2019_SEGGERMEISTERS.ino
+++ StateHacks2019_SEGGERMEISTERS.ino
@@ -1,21 +1,38 @@
+#include <Adafruit_Sensor.h>
+
 #include <DHT.h>
+#include <DHT_U.h>
+
 signed int VoltageInput = A0;
 float vOut = NULL;
 float vIn = NULL;
 float R1 = 30000;
 float R2 = 7500;
 signed int value = NULL;
+
 ///////////////////////////////
+
 signed int Gas = A1;
 float gasValue = NULL;
+
 //////////////////////////
-signed int TempPin = 11;
 
+#define DHTPIN 11
+#define DHTTYPE DHT11
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
+/////////////////////////////////////
 
+signed int VibPin = 9;
+
+////////////////////////////
 void setup() {
   pinMode(VoltageInput, INPUT);
   pinMode(Gas,INPUT);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  pinMode(VibPin, INPUT);
   Serial.begin(9600);
   
 }
@@ -30,8 +47,30 @@ delay(1000);
 ////////////////////////////
 gasValue = analogRead(Gas);
 Serial.println("gas value = ");
-Serial.println(gasValue,2);
+Serial.println(gasValue);
 delay(1000);
 //////////////////////////
+sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  delay(1000);
+  ///////////////////////////////
 
+  signed int vibMeasure = TP_init();
+  Serial.println("Viberation = ");
+  Serial.println(vibMeasure);
+
+}
+
+long TP_init(){
+  delay(10);
+  long measurement=pulseIn (VibPin, HIGH);  
+  return measurement;
 }
